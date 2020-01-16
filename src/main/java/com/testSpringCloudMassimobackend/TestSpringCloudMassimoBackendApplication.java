349a5d06ecfc5c366524032d38c477e9cc5a0d08
AlessandroOlivieri89 <a.olivieri@ecubit.it>
2020-01-16 14:46:41 +0100

    first commit

---
+++ src/main/java/com/testSpringCloudMassimobackend/TestSpringCloudMassimoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.testSpringCloudMassimobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringCloudMassimoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSpringCloudMassimoBackendApplication.class, args);
+	}
+
+}
